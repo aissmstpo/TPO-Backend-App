fcc27520d9b04af06ff2737cd1d5fb12120d2521
miracle16 <kumarmangesh55@gmail.com>
2020-04-17 22:52:53 +0530

    add requirements.txt and test db connection using pymongo

--- run.py
+++ run.py
@@ -1,10 +1,17 @@
 import os
-
+from flask import jsonify
 from app import create_app
+from pymongo import MongoClient
 
 config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
 
+
+@app.route("/")
+def index():
+    client = MongoClient(app.config["DB_URI"])
+    dbs = client.list_database_names()
+    return jsonify(dbs)
 
 if __name__ == '__main__':
     app.run()
