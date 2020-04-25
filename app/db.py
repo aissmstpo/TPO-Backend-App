5b0f94121614edf63d070cc380c81a61e353f4d8
kmangesh49 <kumarmangesh55@gmail.com>
2020-04-25 20:05:47 +0530

    add method to return db instance

--- app/db.py
+++ app/db.py
@@ -1,3 +1,19 @@
-"""
-This module contains all database interfacing methods for the application.
-"""
+from flask import current_app, g
+from werkzeug.local import LocalProxy
+from bson import ObjectId
+from pymongo import MongoClient
+
+def get_db():
+    """
+    method to return db instance
+    """
+    db = getattr(g, "_database", None)
+    DB_URI = current_app.config["DB_URI"]
+    DB_NAME = current_app.config["DB_NAME"]
+    if db is None:
+        db = g._database = MongoClient(
+        DB_URI,
+        )[DB_NAME]
+    return db
+
+
