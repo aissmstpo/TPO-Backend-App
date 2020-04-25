2a4948eb0cc49e03cf417f567bd79d871b16a660
kmangesh49 <kumarmangesh55@gmail.com>
2020-04-25 20:10:16 +0530

    add userDAO methods and users API methods

--- app/api/user.py
+++ app/api/user.py
@@ -1,7 +1,9 @@
 """
 This module contains all API for the users.
 """
-from flask import Blueprint
+from flask import Blueprint,jsonify
+
+from app.dao.usersDAO import get_all_users,get_all_students,get_user_by_id,get_all_companies
 
 user_api_v1 = Blueprint("user_api_v1","user_api_v1",url_prefix="/api/v1/user")
 
@@ -9,48 +11,42 @@ user_api_v1 = Blueprint("user_api_v1","user_api_v1",url_prefix="/api/v1/user")
 def api_get_all_users():
     """
     Get all users details.
+
+    :returns: list of users with details
+    :rtype: list
     """
-    pass
+    return jsonify(get_all_users())
 
 @user_api_v1.route("/student")
 def api_get_all_students():
     """
     Get all student details.
+
     :returns: list of students with details
     :rtype: list
     """
-    pass
+    return jsonify(get_all_students())
 
-@user_api_v1.route("/student/<id>")
-def api_get_student_by_id(id):
+@user_api_v1.route("/<id>")
+def api_get_user_by_id(id):
     """
-    Get a particular student details by his id.
+    Get a particular user details by his id.
     
-    :param id: Id of the student
+    :param id: Id of the user
     :type id: str
-    :return: a dict containing student details
+    :return: a dict containing user details
     :rtype: dict
     """
-    pass
+    return jsonify(get_user_by_id(id))
 
 @user_api_v1.route("/company")
 def api_get_all_companies():
     """
     Get all student details.
+    :returns: a list of companies with details.
+    :rtype: list
     """
-    pass
-
-@user_api_v1.route("/company/<id>")
-def api_get_company_by_id(id):
-    """
-    Get a particular student details by his id.
-    
-    :param id: Id of the particular company
-    :type id: str
-    :return: a dict containing all the company details(company_name,concerned_person_name)
-    :rtype: dict
-    """
-    pass
+    return jsonify(get_all_companies())
 
 @user_api_v1.route("/register")
 def register():
---
+++ app/dao/usersDAO.py
@@ -0,0 +1,32 @@
+from werkzeug.local import LocalProxy
+
+from bson import ObjectId
+
+from app.db import get_db
+
+
+db = LocalProxy(get_db)
+
+def get_all_users():
+    try:
+        return list(db["users"].find({},{"password":0}))
+    except Exception as e:
+        return e
+
+def get_user_by_id(id):
+    try:
+        return db["users"].find_one({"_id":ObjectId(id)},{"password":0})
+    except Exception as e:
+        return e
+
+def get_all_students():
+    try:
+        return list(db["users"].find({"role":"student"},{"password":0}))
+    except Exception as e:
+        return e
+
+def get_all_companies():
+    try:
+        return list(db["users"].find({"role":"company"},{"password":0}))
+    except Exception as e:
+        return e
