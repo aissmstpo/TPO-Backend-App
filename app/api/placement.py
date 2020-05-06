33256004f01461a5636106273a180427e3113031
dheeraj-israni <dheerajisrani12@gmail.com>
2020-05-06 20:02:16 +0530

    Added Documentation in README, placement.py & run.py & added tpo_venv in .gitignore

--- app/api/placement.py
+++ app/api/placement.py
@@ -1,3 +1,6 @@
+"""
+This module contains all API for the placements.
+"""
 from flask import Blueprint
 
 placement_api_v1 = Blueprint("placement_api_v1","placement_api_v1",url_prefix="/api/v1/placement")
